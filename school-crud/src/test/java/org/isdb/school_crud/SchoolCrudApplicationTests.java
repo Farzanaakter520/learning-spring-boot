0841939f505fedb4ce8c879d35a64fd2bbdd06ec
Farzana Akter <farzana.nilu99@gmail.com>
2025-03-24 10:35:35 -0700

    update with new project

---
+++ school-crud/src/test/java/org/isdb/school_crud/SchoolCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isdb.school_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
